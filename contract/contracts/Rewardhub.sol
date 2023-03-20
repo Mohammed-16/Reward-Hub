d065fec992462df3cc48de62560bd0152b9da104
Soham Shah <sohamshah419@gmail.com>
2023-03-21 02:05:36 +0530

    RewardHub contract modified

--- contract/contracts/Rewardhub.sol
+++ contract/contracts/Rewardhub.sol
@@ -1,21 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Rewardhub {
-    
-    mapping(address => uint256) public balances;
-    
-    event Deposit(address indexed depositor, uint256 amount);
-    event Reward(address indexed depositor, uint256 amount, uint256 metadata);
-    
-    function deposit() public payable {
-        require(msg.value == 0, "Invalid deposit amount"); 
-        balances[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
-        
-        if (balances[msg.sender] >= 2 ether) {
-            balances[msg.sender] -= 2 ether;
-            emit Reward(msg.sender, 2 ether, 100); 
-        }
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract RewardHub is ERC721 {
+    uint256 private _tokenIdTracker;
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor() ERC721("ImageNFT", "INFT") {}
+
+    function mint(address to, string memory tokenURI) public returns (uint256) {
+        uint256 newTokenId = _tokenIdTracker;
+        _safeMint(to, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        _tokenIdTracker++;
+        return newTokenId;
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory uri) internal virtual {
+        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = uri;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return _tokenURIs[tokenId];
+    }
+
+    function transferNFT(address _to, uint256 _tokenId) public {
+        require(ownerOf(_tokenId) == msg.sender, "ImageNFT: You do not own this NFT.");
+        safeTransferFrom(msg.sender, _to, _tokenId);
     }
 }
